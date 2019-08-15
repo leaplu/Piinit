cec6ad646da167098fab422f03777b393533450b
leaplu <35611650+leaplu@users.noreply.github.com>
2019-08-15 18:53:52 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#Set mouse
+echo "usbhid.mousepoll=0" >>/boot/cmdline.txt
+
+#Change Sources
+sudo sed -i 's|http://raspbian.raspberrypi.org|https://mirrors.ustc.edu.cn/raspbian|g' /etc/apt/sources.list
+sudo sed -i 's|http://archive.raspberrypi.org|https://mirrors.ustc.edu.cn/archive.raspberrypi.org|g' /etc/apt/sources.list.d/raspi.list
+
+#Update System
+apt update
+apt upgrade -y
+apt install vim -y
